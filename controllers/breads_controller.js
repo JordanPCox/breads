c2a8e39307124d5aaa9313b5997609c5fdeeb283
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-01-21 13:44:19 -0500

    Added page title.

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,11 +2,13 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
+// INDEX
 breads.get('/', (req, res) => {
     res.render('Index',
-         {
-           breads: Bread
-         }
+      {
+        breads: Bread,
+        title: 'Index Page'
+      }
     )
 })
 
