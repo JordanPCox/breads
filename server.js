f8ad5ddd1de8a6aee46ae9cb7496f2700b3a4085
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-01-23 14:57:17 -0500

    Fixed index screen.

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ app.get('/', (req, res) => {
     res.send('Welcome to an awesome app about Breads')
 })
 
+// MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+
 
 const breadsController = require('./controllers/breads_controller')
 app.use('/breads', breadsController)
