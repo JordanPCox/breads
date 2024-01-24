1245ff412842141064e525bf36a3a68e257b6abf
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-01-23 20:35:08 -0500

    Fixed index screen

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,6 +2,15 @@ const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
+// INDEX
+breads.get('/', (req, res) => {
+  res.render('Index',
+    {
+      breads: Bread,
+      title: 'Index Page'
+    }
+  )
+})
 
 // NEW
 breads.get('/new', (req, res) => {
@@ -19,10 +28,6 @@ breads.get('/:arrayIndex', (req, res) => {
   }
 })
 
-
-breads.get('/:arrayIndex', (req, res) => {
-  res.send(Bread[req.params.arrayIndex])
-})
 
 // CREATE
 breads.post('/', (req, res) => {
--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const app = express()
 
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
-app.get('/', (req, res) => {
-    res.send('Welcome to an awesome app about Breads')
-})
-
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+
+app.get('/', (req, res) => {
+    res.send('Welcome to an awesome app about Breads')
+})
+
+
 
 
 const breadsController = require('./controllers/breads_controller')
