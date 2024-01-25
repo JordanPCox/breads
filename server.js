9a38866608a169401bbfe985bfbe2001f004db1f
Jordan P. Cox <cox.jordan.p@gmail.com>
2024-01-25 18:33:13 -0500

    Delete route complete

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+
+// DEPENDENCIES
+const methodOverride = require('method-override')
+
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 // MIDDLEWARE
+app.use(methodOverride('_method'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
@@ -14,8 +19,6 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.get('/', (req, res) => {
     res.send('Welcome to an awesome app about Breads')
 })
-
-
 
 
 const breadsController = require('./controllers/breads_controller')
